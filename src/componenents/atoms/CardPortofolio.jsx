ee6db5e44fc935274c1e33d185b35c719d08ffe1
RezalMaksmi <rezalnurscc@gmail.com>
2024-09-17 13:39:22 +0700

    update porto

--- src/componenents/atoms/CardPortofolio.jsx
+++ src/componenents/atoms/CardPortofolio.jsx
@@ -12,11 +12,64 @@ import bukapedia from "../../assets/portofolio/bukapedia.jpg";
 import belanjamudah from "../../assets/portofolio/belanjamudah.png";
 import mbkm from "../../assets/portofolio/mbkm.png";
 import mindes from "../../assets/portofolio/mindes.png";
+import bumdes from "../../assets/portofolio/bumdes.png";
+import kopi from "../../assets/portofolio/kopi.png";
 import { BiLinkExternal } from "react-icons/bi";
 
 const CardPortofolio = () => {
   return (
     <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 overflow-y-scroll py-3 px-3 md:gap-8 gap-4 justify-start  w-full">
+      <div className="flex flex-col  p-2 gap-3 rounded-lg relative">
+        <img
+          src={bumdes}
+          alt=""
+          className="h-[250px] min-w-[190px] rounded-lg object-cover object-top"
+        />
+        <div className="flex flex-col text-white absolute bottom-0 backdrop-blur-sm p-3 bg-[#1e1e1e86]">
+          <h5 className="text-2xl">Bumdes</h5>
+          <span className="text-sm ">
+            Pengelolaan Data Excel Berbasis Website
+          </span>
+          <span className="text-sm ">Role : Frontend</span>
+
+          <div className="flex flex-row gap-1  mt-2 w-full">
+            <img src={react} alt="" className="object-contain w-6" />
+            <img src={tailwindcss} alt="" className="object-contain w-6" />
+          </div>
+          <a
+            href="https://bumdestirtomajubersama.com/"
+            className="text-white text-base px-3 py-1 bg-[#707070] absolute  top-0 right-0 rounded-sm"
+          >
+            <BiLinkExternal />
+          </a>
+        </div>
+      </div>
+
+      <div className="flex flex-col  p-2 gap-3 rounded-lg relative">
+        <img
+          src={kopi}
+          alt=""
+          className="h-[250px] min-w-[190px] rounded-lg object-cover object-top"
+        />
+        <div className="flex flex-col text-white absolute bottom-0 backdrop-blur-sm p-3 bg-[#1e1e1e86]">
+          <h5 className="text-2xl">HinggaPagi</h5>
+          <span className="text-sm ">Website HinggaPagi - fastwork</span>
+          <span className="text-sm ">Role : Frontend</span>
+
+          <div className="flex flex-row gap-1  mt-2 w-full">
+            <img src={html} alt="" className="object-contain w-6" />
+            <img src={css} alt="" className="object-contain w-6" />
+            <img src={tailwindcss} alt="" className="object-contain w-6" />
+          </div>
+          <a
+            href="https://hinggapagi.com/"
+            className="text-white text-base px-3 py-1 bg-[#707070] absolute  top-0 right-0 rounded-sm"
+          >
+            <BiLinkExternal />
+          </a>
+        </div>
+      </div>
+
       <div className="flex flex-col  p-2 gap-3 rounded-lg relative">
         <img
           src={starflix}
